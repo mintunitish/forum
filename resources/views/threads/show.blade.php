aacda55532f1ffb148b2c204773c482c2b1f4390
Nitish Kumar <mintu.nitish@gmail.com>
2018-07-30 02:35:03 +0530

    Threads can have replies
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,11 +6,27 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ $thread->title }}</div>
-
                     <div class="card-body">
                         {{ $thread->body }}
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                    <div class="card">
+                        <div class="card-header">
+                            <a href="#">
+                                <i>{{ $reply->owner->name }}</i>
+                            </a>
+                            said {{ $reply->created_at->diffForHumans() }}...
+                        </div>
+                        <div class="card-body">
+                            {{ $reply->body }}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nitish Kumar
+ * Date: 7/30/2018
+ * Time: 12:22 AM
+ */
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function a_user_can_view_single_thread()
+    {
+        $this->get('/threads/'. $this->thread->id )
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/'. $this->thread->id )
+            ->assertSee($reply->body);
+    }
+}
