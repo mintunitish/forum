4afbf37b2b33b8b4b6449c98f1206920611e1908
Nitish Kumar <mintu.nitish@gmail.com>
2018-08-01 16:15:46 +0530

    Dusk Login Tests
    Signed-off-by: Nitish Kumar <nitish.kumar@sugaldamani.com>

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * @throws \Throwable
+     */
+    public function test_a_registered_user_can_log_in()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'tmastas@jasmdfn.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+    }
+
+    public function test_non_registered_users_can_not_log_in()
+    {
+        $this->browse(function ($browser) {
+            $browser->visit('/login')
+                ->type('email', 'some@random.guy')
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/login');
+        });
+    }
+
+    // TODO: For register page tests
+}
