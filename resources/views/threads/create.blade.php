9b2ef273c5d793f133748d72b0f187dcdd4b71f4
Nitish Kumar <mintu.nitish@gmail.com>
2018-08-04 22:19:09 +0530

    Validations Error & Old Data
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,15 +10,33 @@
                     <div class="card-body">
                         <form action="/threads" method="post">
                             {{ csrf_field() }}
+                            @if($errors)
+                                @foreach($errors->all() as $error)
+                                    <div class="alert alert-danger" role="alert">
+                                        {{ $error }}
+                                    </div>
+                                @endforeach
+                            @endif
+                            <div class="form-group">
+                                <label for="channel_id">Choose a channel:</label>
+                                <select name="channel_id" id="channel_id" class="form-control" required>
+                                    <option value="">Choose One</option>
+                                    @foreach(App\Channel::all() as $channel)
+                                        <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" class="form-control" id="title" placeholder="Title" name="title">
+                                <input type="text" class="form-control" id="title" placeholder="Title" value="{{ old('title') }}" name="title" required>
                             </div>
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea class="form-control" id="body" rows="8" name="body" placeholder="body"></textarea>
+                                <textarea class="form-control" id="body" rows="8" name="body" required>{{ old('body') }}</textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary">Publish</button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
                         </form>
                     </div>
                 </div>
